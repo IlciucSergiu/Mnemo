fad30b9226703f8d3fc86936c28dea5933dae3be
Sergiu17 <eu_sergiuu14@yahoo.com>
2015-05-11 22:46:14 +0300

    Inceputul formei1

--- MemoTricks/Program.cs
+++ MemoTricks/Program.cs
@@ -16,7 +16,7 @@ namespace MemoTricks
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SistemulPeg());
         }
     }
 }
